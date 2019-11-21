77c5007aa85f8d52ae7098092cef91aa6970fc7d
Ben Fox <benfox1216@gmail.com>
2019-11-21 14:35:27 -0700

    Complete Iteration 1

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,11 +1,11 @@
 ages = [24, 30, 18, 20, 41]
 
-sum = 0
+sum1 = 0
 ages.each do |age|
-  sum += age
+  sum1 += age
 end
 
-p sum
+p sum1
 
 number = 0
 ages.each do |age|
@@ -14,20 +14,31 @@ end
 
 p number
 
-p mean = sum/number.to_f
+p mean1 = sum1/number.to_f
 
 new_array1 = []
 ages.each do |age|
-  subtract = age - mean
+  subtract = age - mean1
   new_array1 << subtract.round(2)
 end
 
 p new_array1
 
 new_array2 = []
-new_array1.each do |place_holder|
-  place_holder * place_holder
-  new_array2 << place_holder
+new_array1.each do |place_holder1|
+  place_holder2 = place_holder1 * place_holder1
+  new_array2 << place_holder2.round(2)
 end
 
 p new_array2
+
+sum2 = 0
+new_array2.each do |place_holder|
+  sum2 += place_holder
+end
+
+p sum2.round(2)
+
+p mean2 = sum2.round(2)/number
+
+p std_dev = Math.sqrt(mean2).round(2)
