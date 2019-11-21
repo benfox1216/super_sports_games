1ee75ebaae380a8be05620b4b0df5b48ef1541c4
Ben Fox <benfox1216@gmail.com>
2019-11-21 14:17:41 -0700

    Complete step 4, on step 5

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -14,4 +14,20 @@ end
 
 p number
 
-p mean = sum.to_f/number
+p mean = sum/number.to_f
+
+new_array1 = []
+ages.each do |age|
+  subtract = age - mean
+  new_array1 << subtract.round(2)
+end
+
+p new_array1
+
+new_array2 = []
+new_array1.each do |place_holder|
+  place_holder * place_holder
+  new_array2 << place_holder
+end
+
+p new_array2
